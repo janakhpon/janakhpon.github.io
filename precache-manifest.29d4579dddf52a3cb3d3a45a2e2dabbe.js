d3852d177f578605b0938660e5ab7e26e6af7dbb
janakhpon <jnovaxer@gmail.com>
2019-08-28 17:31:13 +0630

    Updates

---
+++ precache-manifest.29d4579dddf52a3cb3d3a45a2e2dabbe.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "270aff67761359aa97152578c91abad5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7c3b239fa2affde5e7ea",
+    "url": "/static/css/main.c3613413.chunk.css"
+  },
+  {
+    "revision": "37b0e3aadfc1ada4502c",
+    "url": "/static/js/2.dd74596b.chunk.js"
+  },
+  {
+    "revision": "7c3b239fa2affde5e7ea",
+    "url": "/static/js/main.939716dd.chunk.js"
+  },
+  {
+    "revision": "fcafff3fa400dcc0789b",
+    "url": "/static/js/runtime~main.86daef54.js"
+  }
+]);
