15714f26c673ee156e0e5c8b66f604f3edc87c03
janakhpon <jnovaxer@gmail.com>
2020-02-08 14:06:20 +0630

    GDEV036 solved fonts issues

--- src/theme.js
+++ src/theme.js
@@ -1,27 +1,5 @@
 import theme from 'styled-theming';
 
-export const backgroundColor = theme('mode', {
-  light: '#fafafa',
-  dark: '#222'
-});
-
-export const textColor = theme('mode', {
-  light: '#000',
-  dark: '#fff'
-});
-
-export const buttonBackgroundColor = theme('mode', {
-  light: '#222',
-  dark: '#eee'
-});
-
-export const buttonTextColor = theme('mode', {
-  light: '#eee',
-  dark: '#222'
-});
-
-
-
 export const navbackgroundColor = theme('mode', {
   light: '#241e3c',
   dark: '#242029'
@@ -46,4 +24,25 @@ export const containertextColor = theme('mode', {
 export const switchValue = theme('mode', {
   light: false,
   dark: true
+})
+
+export const aofsubheadingdiv = theme('mode', {
+  light: '#251e3e',
+  dark: '#ff0000'
+})
+
+export const subheadtextColor = theme('mode', {
+  light: '#353a40',
+  dark: '#F3FFBD'
+})
+
+export const spantextColor = theme('mode', {
+  light: '#247BA0',
+  dark: '#FF1654'
+})
+
+
+export const titletextColor = theme('mode', {
+  light: '#353a40',
+  dark: '#05668D'
 })
