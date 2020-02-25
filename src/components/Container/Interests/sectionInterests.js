52e34a76681445bc56be9b24b27a339f7d7b83d6
janakhpon <jnovaxer@gmail.com>
2020-02-25 17:31:06 +0630

    GDEV046 corrected grammical and spelling mistakes on resume(oh! how dump of me ;) hehe)

--- src/components/Container/Interests/sectionInterests.js
+++ src/components/Container/Interests/sectionInterests.js
@@ -15,8 +15,10 @@ export default class Interests extends Component {
             <section className="resume-section p-3 p-lg-5 d-flex align-items-center" id="interests">
                 <Head className="w-100">
                     <h2 className="mb-5">Interests</h2>
-                    <p>Apart from being a lead developer, I enjoy most of my time being outdoors. In the winter, I enjoy sightseeing in the forest with a camera as i love shooting awesome photo for my photography album.</p>
-                    <p className="mb-0">When forced indoors, I love reading, writing poems next to backyard's window and watching anime.</p>
+                    <p>Apart from being a lead developer, I enjoy most of my time being outdoors. In the cool season (late October to mid-February in Myanmar), I enjoy sightseeing in the forest with a camera as i love shooting awesome photo for my photography album.</p>
+                    <p className="mb-0">When forced indoors, I love reading novels and tech news, writing poems next to backyard's window and watching animes (One Piece, Naruto ...).</p>
+                    <p className="mb-0">I also have a habbit continuos learning for new technology which could mesmerize me (Currently Golang and AI) . </p>
+
                 </Head>
             </section>
         );
