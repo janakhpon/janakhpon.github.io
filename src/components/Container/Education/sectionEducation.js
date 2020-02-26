5f3ffca26d06f3d00627a390895a41495a9eab94
janakhpon <jnovaxer@gmail.com>
2020-02-27 05:03:00 +0630

    GDEV048 updated all the neccessary information and fixes 🥴 🥴

--- src/components/Container/Education/sectionEducation.js
+++ src/components/Container/Education/sectionEducation.js
@@ -21,8 +21,8 @@ export default class Education extends Component {
 
                     <div className="resume-item d-flex flex-column flex-md-row justify-content-between mb-5">
                         <div className="resume-content">
-                            <h3 className="mb-0">Technological University of Mawlamyine</h3>
-                            <div className="subheading mb-3">Bachelor of Engineering </div>
+                            <h3 className="mb-0">Technological University( Mawlamyine )</h3>
+                            <div className="subheading mb-3">Bachelor of Engineering (BE.IT)</div>
                             <div>Specialized at Information Technology</div>
                             {
                                 /**
